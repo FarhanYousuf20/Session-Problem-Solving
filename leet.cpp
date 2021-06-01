76ee7d5e533cbd51456b8b4b49b379482309cc7d
FarhanYousuf20 <farhanyousuf2019@gmail.com>
2021-06-02 00:04:24 +0600

    leet

--- leet.cpp
+++ leet.cpp
@@ -1,2 +1,20 @@
-ahsiq
-asd
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        if(!grid[0][0]) dp[0][0] = 1;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j]) continue;
+                if(i - 1 >= 0) dp[i][j] += dp[i - 1][j];
+                if(j - 1 >= 0) dp[i][j] += dp[i][j - 1];
+            }
+        }
+        
+        return dp[n - 1][m - 1];
+    }
+};
